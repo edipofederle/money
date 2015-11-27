eae4097d796beeb3360651170216ec126d4fa24d
Edipo L Federle <edipofederle@gmail.com>
2015-11-27 13:27:02 -0200

    Configure base rate and currencies

--- lib/money.rb
+++ lib/money.rb
@@ -1,5 +1,13 @@
 require "money/version"
 
 class Money
-  # Your code goes here...
+
+  class << self
+    attr_accessor :base_rate, :currencies_rates
+  end
+  
+  def self.conversion_rates(base_rate, currencies_rates)
+    self.base_rate        = base_rate
+    self.currencies_rates = currencies_rates
+  end
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe Money do
+  
   it 'has a version number' do
     expect(Money::VERSION).to eq "0.1.0"
+  end
+
+  it "has initial configurations" do
+    Money.conversion_rates('EUR', {'USD' => 1.11, 'Bitcoind' => 0.0047})
+
+    expect(Money.base_rate).to eq 'EUR'
   end
 
 end
