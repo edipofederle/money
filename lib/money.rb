4a7707bfa72946aedf1e4c5b32496b2140d54e2e
Edipo L Federle <edipofederle@gmail.com>
2015-11-27 13:32:13 -0200

    Minors. Instantiate Money Objects

--- lib/money.rb
+++ lib/money.rb
@@ -2,10 +2,16 @@ require "money/version"
 
 class Money
 
+  def initialize(value, currency)
+    @value    = value
+    @currency = currency
+  end
+
   class << self
     attr_accessor :base_rate, :currencies_rates
   end
-  
+
+  # Configure the currency rates with respect to a base currency (here EUR):
   def self.conversion_rates(base_rate, currencies_rates)
     self.base_rate        = base_rate
     self.currencies_rates = currencies_rates
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -6,10 +6,16 @@ describe Money do
     expect(Money::VERSION).to eq "0.1.0"
   end
 
-  it "has initial configurations" do
+  it 'has initial configurations' do
     Money.conversion_rates('EUR', {'USD' => 1.11, 'Bitcoind' => 0.0047})
 
     expect(Money.base_rate).to eq 'EUR'
+  end
+
+  it 'is a instance of Money' do
+    fifity_eur = Money.new(50, 'EUR')
+    
+    expect(fifity_eur).to be_an_instance_of Money
   end
 
 end
